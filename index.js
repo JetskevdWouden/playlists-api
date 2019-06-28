7b5eb74f77d65110a89ef17be1e6efc831ee5080
JetskevdWouden <zegmaarjet@gmail.com>
2019-06-28 21:38:10 +0200

    Create Playlist TB / model

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require('express');
-const app = express();
 const db = require('./db')
+
+//MODELS
+const playlist = require('./playlist/model');
+
+//ROUTERS
+
+
+
+
+//CREATING OUR SERVER
+const app = express();
+
+//CREATING A BODY-PARSER OBJECT
+
+
+//CONFIGURING OUR SERVER
+//app.use
+
+
 
 const port = process.env.PORT || 4000;
 
+//OPENING SERVER FOR TRAFFIC
 app.listen(port, () => console.log(`Listening on port ${port}`))
---
+++ playlist/model.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Playlist = db.define(
+    'playlist',
+    {
+        name: {
+            type: Sequelize.STRING,
+            field: 'playlist_name'
+        }
+    },
+    { tableName: 'playlists'}
+)
+
+module.exports = Playlist;
